16394aa0d4392fd7efd96c2d47ec2b9f3ace32d9
rosintaa5 <rosintaanggraini85@gmail.com>
2026-06-29 10:36:18 +0700

    Initialize socket connection using socket.io-client

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,1 +1,10 @@
-export const socket = null;
+import { io } from "socket.io-client";
+
+export const socket = io(
+  "https://confident-tranquility-production-ceaa.up.railway.app",
+  {
+    transports: ["websocket", "polling"],
+    forceNew: true,
+    reconnection: true
+  }
+);
